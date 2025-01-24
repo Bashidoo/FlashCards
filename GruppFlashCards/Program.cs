ba7a93ff6c6a12c412edb603c152faf5fab78c00
Busher <besher.abodan@icloud.com>
2025-01-24 13:41:04 +0100

    Program.cs

---
+++ GruppFlashCards/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GruppFlashCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
