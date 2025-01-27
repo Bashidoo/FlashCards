d28b6bde2a81da195e678835f1789caaf9139825
Busher <besher.abodan@icloud.com>
2025-01-27 17:33:37 +0100

    Added Category class
    Category class to allow ER Diagram to be in third normal form 3RF:

---
+++ GruppFlashCards/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppFlashCards
+{
+    public class Category
+    {
+
+        public List<FlashCard> flashCardID = new List<FlashCard>();
+
+        public int categoryID { get; set; };
+
+        public string categoryName { get; set; }
+        public Category()
+        {
+            
+        }
+    }
+}
--- GruppFlashCards/Menu.cs
+++ GruppFlashCards/Menu.cs
@@ -8,6 +8,6 @@ namespace GruppFlashCards
 {
     public class Menu
     {
-
+        // Add Logic
     }
 }
